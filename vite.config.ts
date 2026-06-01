d8c5a66b60c2977ed3497fd55834af7c811a9f33
kenmoh <kenneth.aremoh@gmail.com>
2026-06-01 16:30:44 +0100

    configure vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from 'nitro/vite';
 
 export default defineConfig({
   server: {
@@ -17,8 +18,5 @@ export default defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
-  plugins: [
-    tanstackStart(),
-    viteReact(),
-  ],
+ plugins: [tanstackStart(), nitro(), viteReact()],
 });
